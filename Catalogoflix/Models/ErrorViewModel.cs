94f80dedb09e658c5077586dc357a71456ade641
Leonardo <leonardo@sarival.com.br>
2019-03-04 18:22:24 -0300

    Add project files.

---
+++ Catalogoflix/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Catalogoflix.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
